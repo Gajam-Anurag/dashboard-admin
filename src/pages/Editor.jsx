c074a882d3cd822394c85678b3d616e599f6ebde
Anurag Gajam <37669266+AnuragGajam@users.noreply.github.com>
2022-06-16 13:48:46 +0530

    6th phase implementation

--- src/pages/Editor.jsx
+++ src/pages/Editor.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
 
+import { HtmlEditor, Inject, Link, Toolbar, Image, QuickToolbar, RichTextEditorComponent } from '@syncfusion/ej2-react-richtexteditor'
+
+import {EditorData} from '../data/dummy';
+import {Header} from '../components';
+
 const Editor = () => {
   return (
-    <div>Editor</div>
+    <div className='m-2 p-2 mt-24 md:m-10 md:p-10 bg-white rounded-3xl'>
+      <Header category='App' title='Editor' />
+      <RichTextEditorComponent id='editor'>
+        <EditorData />
+        <Inject services={[HtmlEditor, Toolbar, Image, Link, QuickToolbar ]}/>
+      </RichTextEditorComponent>
+    </div>
   )
 }
 
--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -1,5 +1,5 @@
 import React, {useEffect, useState} from 'react';
-import { GridComponent, ColumnsDirective, ColumnDirective, Resize, Sort, ContextMenu, Filter, Page, ExcelExport, PdfExport, Edit, Inject, Toolbar }
+import { GridComponent, ColumnsDirective, ColumnDirective, Resize, Sort, ContextMenu, Filter, Page, ExcelExport, PdfExport, Edit, Inject, Toolbar, gridActionHandler }
  from '@syncfusion/ej2-react-grids';
 
 import { ordersData, contextMenuItems, ordersGrid } from '../data/dummy'; 
@@ -10,11 +10,15 @@ import { Header } from '../components';
 
 const Orders = () => {
 
+  const toolbarClick = (args) => {  
+    if (this.grid && args.item.id === 'grid_pdfexport') {
+      this.grid.pdfExport();
+      }}
 
   return (
     <div className='m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl'>
       <Header category='Page' title="Orders"/>
-      <GridComponent id="gricomp" dataSource={ordersData} allowPaging={true} allowSorting={true} allowPdfExport={true} toolbar='PdfExport' >
+      <GridComponent id="gricomp" dataSource={ordersData} allowPaging allowSorting allowPdfExport toolbar={['PdfExport']} toolbarClick='toolbarClickHandler' >
         <ColumnsDirective>
           { ordersGrid.map((item, index) => (
             <ColumnDirective key={index}{...item}/>
